675cf4e56d29f0734a7fbd07707fa28fde79d1ee
Ben Herila <ben@herila.net>
2012-10-23 13:31:54 -0700

    updated zlib to zlib-1.2.7

--- contrib/masmx64/bld_ml64.bat
+++ contrib/masmx64/bld_ml64.bat
@@ -1,2 +1,2 @@
-ml64.exe /Flinffasx64 /c /Zi inffasx64.asm
-ml64.exe /Flgvmat64   /c /Zi gvmat64.asm
+ml64.exe /Flinffasx64 /safeseh /c /Zi inffasx64.asm
+ml64.exe /Flgvmat64   /safeseh /c /Zi gvmat64.asm
--- contrib/masmx86/bld_ml32.bat
+++ contrib/masmx86/bld_ml32.bat
@@ -1,2 +1,2 @@
-ml /coff /safeseh /Zi /c /Flmatch686.lst match686.asm
-ml /coff /safeseh /Zi /c /Flinffas32.lst inffas32.asm
+ml /coff /Zi /safeseh /c /Flmatch686.lst match686.asm
+ml /coff /Zi /safeseh /c /Flinffas32.lst inffas32.asm
