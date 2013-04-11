e9f3e9a460ede80b286a0e35db2960a2db202681
Tim Rogers <tiroger@microsoft.com>
2013-04-11 08:33:18 -0700

    build corrections, coapp file relocation, and .autopkg update

---
+++ contrib/coapp/all.bat
@@ -0,0 +1,44 @@
+@echo off
+setlocal
+
+if "%1"=="clean" goto :clean
+if "%1"=="noclean" (
+	set __NOCLEAN__=true
+	shift)
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" amd64
+call :build x64 Release v110
+endlocal
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+call :build Win32 Release v110
+endlocal
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
+call :build x64 Release v100
+endlocal
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+call :build Win32 Release v100
+endlocal
+
+if "__NOCLEAN__"=="true" goto :eof
+
+goto :clean
+
+:build
+msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=DynamicLibrary /P:CallingConvention=cdecl .\zlib.vcxproj
+msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=StaticLibrary /P:CallingConvention=cdecl .\zlib.vcxproj
+msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=DynamicLibrary /P:CallingConvention=stdcall .\zlib.vcxproj
+msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=StaticLibrary /P:CallingConvention=stdcall .\zlib.vcxproj
+goto :eof
+
+:clean
+REM rd /s /q v90
+rd /s /q v100
+rd /s /q v110
+
