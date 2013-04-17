88c2a3558cc795ee4ed7a803825516b49bd2fe0d
Tim Rogers <tiroger@microsoft.com>
2013-04-17 11:05:08 -0700

    Corrections and adjusted to also build and include debug files

--- contrib/coapp/all.bat
+++ contrib/coapp/all.bat
@@ -9,21 +9,25 @@ if "%1"=="noclean" (
 setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" amd64
 call :build x64 Release v110
+call :build x64 Debug v110
 endlocal
 
 setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
 call :build Win32 Release v110
+call :build Win32 Debug v110
 endlocal
 
 setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
 call :build x64 Release v100
+call :build x64 Debug v100
 endlocal
 
 setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
 call :build Win32 Release v100
+call :build Win32 Debug v100
 endlocal
 
 if "__NOCLEAN__"=="true" goto :eof
