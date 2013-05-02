a90d68aade476484271a627224e1b68988436b53
Tim Rogers <tiroger@microsoft.com>
2013-05-02 10:04:07 -0700

    corrections to build files

--- contrib/coapp/all.bat
+++ contrib/coapp/all.bat
@@ -8,26 +8,26 @@ if "%1"=="noclean" (
 
 setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" amd64
-call :build x64 Release v110
-call :build x64 Debug v110
+call :build x64 Release v110 || goto :eof
+call :build x64 Debug v110 || goto :eof
 endlocal
 
 setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
-call :build Win32 Release v110
-call :build Win32 Debug v110
+call :build Win32 Release v110 || goto :eof
+call :build Win32 Debug v110 || goto :eof
 endlocal
 
 setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
-call :build x64 Release v100
-call :build x64 Debug v100
+call :build x64 Release v100 || goto :eof
+call :build x64 Debug v100 || goto :eof
 endlocal
 
 setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
-call :build Win32 Release v100
-call :build Win32 Debug v100
+call :build Win32 Release v100 || goto :eof
+call :build Win32 Debug v100 || goto :eof
 endlocal
 
 if "%__NOCLEAN__%"=="true" goto :eof
@@ -35,10 +35,10 @@ if "%__NOCLEAN__%"=="true" goto :eof
 goto :clean
 
 :build
-msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=DynamicLibrary /P:CallingConvention=cdecl .\zlib.vcxproj
-msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=StaticLibrary /P:CallingConvention=cdecl .\zlib.vcxproj
-msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=DynamicLibrary /P:CallingConvention=stdcall .\zlib.vcxproj
-msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=StaticLibrary /P:CallingConvention=stdcall .\zlib.vcxproj
+msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=DynamicLibrary /P:CallingConvention=cdecl .\zlib.vcxproj || goto :eof
+msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=StaticLibrary /P:CallingConvention=cdecl .\zlib.vcxproj || goto :eof
+msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=DynamicLibrary /P:CallingConvention=stdcall .\zlib.vcxproj || goto :eof
+msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=StaticLibrary /P:CallingConvention=stdcall .\zlib.vcxproj || goto :eof
 goto :eof
 
 :clean
